21d7610b3830fb8c0ddec3f7cb6b9709433a848a
Drew Schmidt <wrathematics@gmail.com>
2019-09-29 15:26:46 -0400

    add some examples

---
+++ examples/spvec.cpp
@@ -0,0 +1,15 @@
+#include <spvec.hpp>
+
+
+int main()
+{
+  const int len = 5;
+  spvec<int, int> x(len);
+  x.insert(3, 1);
+  x.insert(1, 2);
+  
+  x.print();
+  x.print(true);
+  
+  return 0;
+}
