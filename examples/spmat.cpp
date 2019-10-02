197e2069a9f2a76e9e35f5c5903f8c6e243f5df6
Drew Schmidt <wrathematics@gmail.com>
2019-10-01 20:40:34 -0400

    add spmat example

---
+++ examples/spmat.cpp
@@ -0,0 +1,29 @@
+#include <spmat.hpp>
+#include <spvec.hpp>
+
+
+int main()
+{
+  const int len = 5;
+  spvec<int, int> v(len);
+  v.insert(3, 1);
+  v.insert(1, 2);
+  
+  spmat<int, int> x(10, 3, len);
+  x.insert(0, v);
+  
+  v.zero();
+  v.insert(5, 3);
+  v.insert(8, 4);
+  // v.insert(2, 5);
+  x.insert(1, v);
+  
+  v.zero();
+  v.insert(7, 5);
+  x.insert(2, v);
+  
+  x.print();
+  x.print(true);
+  
+  return 0;
+}
