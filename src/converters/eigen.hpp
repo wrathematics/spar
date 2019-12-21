023ed0f047f089fb59ab21fb97be3333b427261d
Drew Schmidt <wrathematics@gmail.com>
2019-12-21 08:53:33 -0500

    add some eigen converters

---
+++ src/converters/eigen.hpp
@@ -0,0 +1,95 @@
+// This file is part of spvec which is released under the Boost Software
+// License, Version 1.0. See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef SPVEC_CONVERTERS_EIGEN_H
+#define SPVEC_CONVERTERS_EIGEN_H
+#pragma once
+
+
+#include <cstdint>
+
+#include <Eigen/SparseCore>
+
+#include "../arraytools/src/arraytools.hpp"
+#include "../core/dvec.hpp"
+#include "../core/spmat.hpp"
+#include "../core/spvec.hpp"
+
+
+namesace spvec
+{
+  namespace conv
+  {
+    namespace
+    {
+      template <typename INDEX, typename SCALAR>
+      using eigen_t = Eigen::SparseMatrix<SCALAR, Eigen::StorageOptions::ColMajor, INDEX>;
+    }
+    
+    
+    
+    template <typename INDEX, typename SCALAR>
+    static inline spmat spmat_to_eigen(const spmat<INDEX, SCALAR> &x)
+    {
+      const INDEX m = x.nrows();
+      const INDEX n = x.ncols();
+      const INDEX nnz = x.get_nnz();
+      
+      eigen_t<INDEX, SCALAR> s(m, n);
+      s.makeCompressed();
+      s.resizeNonZeros(nnz);
+      
+      arraytools::copy(nnz, x.data_ptr(), s.valuePtr());
+      arraytools::copy(nnz, x.index_ptr(), s.innerIndexPtr());
+      arraytools::copy(n+1, x.col_ptr(), s.outerIndexPtr());
+      
+      return s;
+    }
+    
+    
+    
+    template <typename INDEX, typename SCALAR>
+    static inline SEXP spmat_to_s4(const eigen_t<INDEX, SCALAR> &s)
+    {
+      const INDEX m = s.rows();
+      const INDEX n = s.cols();
+      const INDEX nnz = s.nonZeros();
+      const INDEX p_len = s.outerSize();
+      
+      spmat<INDEX, SCALAR> x(m, n, nnz);
+      
+      arraytools::copy(nnz, s.valuePtr(), x.data_ptr());
+      arraytools::copy(nnz, s.innerIndexPtr(), x.index_ptr());
+      arraytools::copy(p_len, s.outerIndexPtr(), x.col_ptr());
+      
+      return x;
+    }
+    
+    
+    
+    template <typename INDEX, typename SCALAR>
+    static inline dvec eigen_to_dvec(const INDEX col, const eigen_t<INDEX, SCALAR> &s)
+    {
+      const INDEX m = s.rows();
+      const INDEX n = s.cols();
+      const INDEX nnz = s.nonZeros();
+      
+      const SCALAR *X = spmat_X(s);
+      const INDEX *I = spmat_I(s);
+      const INDEX *P = spmat_P(s);
+      
+      dvec<INDEX, SCALAR> d(m);
+      
+      const INDEX start = P[col];
+      const INDEX end = (col == n) ? nnz : P[col+1];
+      for (INDEX i=start; i<end; i++)
+        d[ I[i] ] = X[i];
+      
+      return d;
+    }
+  }
+}
+
+
+#endif
