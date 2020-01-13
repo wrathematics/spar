393afda282e772a676bda89802ff4cac04aa225e
Drew Schmidt <wrathematics@gmail.com>
2020-01-13 16:10:15 -0500

    add an mpi example

---
+++ examples/mpi/spmat.cpp
@@ -0,0 +1,26 @@
+#include <gen/gen.hpp>
+#include <spar.hpp>
+#include <reduce.hpp>
+
+
+int main()
+{
+  spar::mpi::init();
+  int rank = spar::mpi::get_rank();
+  
+  using INDEX = int;
+  using SCALAR = int;
+  const uint32_t seed = 1234;
+  
+  auto x = spar::gen::gen<INDEX, SCALAR>(seed + rank, 10, 8);
+  if (rank == 0)
+    x.print();
+  
+  auto y = spar::reduce::gather<spmat<INDEX, SCALAR>, INDEX, SCALAR>(0, x);
+  if (rank == 0)
+    y.print();
+  
+  spar::mpi::finalize();
+  
+  return 0;
+}
