6f5dec7d594488ffda138115cfc30fcac8023afd
wrathematics <wrathematics@gmail.com>
2020-04-04 11:40:45 -0400

    add banded generator

--- src/gen/gen.hpp
+++ src/gen/gen.hpp
@@ -90,6 +90,90 @@ namespace spar
       const uint32_t seed = spar::internals::rand::get_seed();
       return bandish<INDEX, SCALAR>(seed, nrows, ncols);
     }
+    
+    
+    
+    namespace internals
+    {
+      template <typename INDEX>
+      static inline INDEX ntri(const INDEX n)
+      {
+        return (n*(n+1))/2;
+      }
+      
+      template <typename INDEX>
+      static inline INDEX banded_nnz(const INDEX band, const INDEX nrows,
+        const INDEX ncols)
+      {
+        const INDEX m = nrows>ncols?nrows:ncols;
+        const INDEX n = nrows>ncols?ncols:nrows;
+        
+        INDEX nnz = n + ntri(n-1)-ntri(n-band) + ntri(m-1)-ntri(m-band) - (m-1-n);
+        if (m == n)
+          nnz -= (m-1 - n);
+        
+        return nnz;
+      }
+    }
+    
+    /**
+      @brief Generate a banded matrix. Non-zero values are generated at
+      random with decreasing probability the farther from the diagonal they are.
+      
+      @param[in] band Size of the band.
+      @param[in] nrows,ncols The dimensions of the return.
+      
+      @return A sparse matrix.
+      
+      @allocs The return matrix is allocated/resized as necessary.
+      
+      @except If a memory allocation fails, a `bad_alloc` exception will be
+      thrown.
+     */
+    template <typename INDEX, typename SCALAR>
+    static inline spmat<INDEX, SCALAR> banded(const INDEX band,
+      const INDEX nrows, const INDEX ncols)
+    {
+      const INDEX nnz = internals::banded_nnz(band, nrows, ncols);
+      spmat<INDEX, SCALAR> x(nrows, ncols, nnz);
+      
+      INDEX *I = x.index_ptr();
+      INDEX *P = x.col_ptr();
+      SCALAR *X = x.data_ptr();
+      
+      INDEX pos = 0;
+      INDEX col_pos = 1;
+      INDEX i = 0, j = 0;
+      
+      P[0] = 0;
+      
+      while (i < nrows)
+      {
+        if (i == j || (i < j && j < i + band) || (i > j && i < j + band))
+        {
+          I[pos] = i;
+          X[pos] = 1;
+          pos++;
+          i++;
+        }
+        else if (i >= band + j)
+        {
+          i = (i>band?i-band:0);
+          j++;
+          
+          P[col_pos] = pos;
+          col_pos++;
+          
+          if (j >= ncols)
+            break;
+        }
+        else
+          i++;
+      }
+      
+      x.update_nnz();
+      return x;
+    }
   }
 }
 
