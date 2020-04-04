28f357bb202a1cf5b6ef5469d5d3c82a4291194e
wrathematics <wrathematics@gmail.com>
2020-04-04 11:43:13 -0400

    update generator example

--- examples/core/gen.cpp
+++ examples/core/gen.cpp
@@ -4,8 +4,19 @@
 
 int main()
 {
-  auto x = spar::gen::gen<int, int>(1234, 10, 10);
+  printf("## A banded matrix\n");
+  auto x = spar::gen::banded<int, int>(2, 10, 5);
+  
+  x.info();
   x.print();
+  x.print(true);
+  
+  printf("## A band-ish matrix\n");
+  auto y = spar::gen::bandish<int, int>(1234, 10, 5);
+  
+  y.info();
+  y.print();
+  y.print(true);
   
   return 0;
 }
