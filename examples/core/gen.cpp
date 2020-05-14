00d2c285328ce5876007976ed90fc3eaf2febe28
wrathematics <wrathematics@gmail.com>
2020-05-14 16:07:17 -0400

    add rand to gen example

--- examples/core/gen.cpp
+++ examples/core/gen.cpp
@@ -4,19 +4,26 @@
 
 int main()
 {
-  printf("## A banded matrix\n");
-  auto x = spar::gen::banded<int, int>(2, 10, 5);
+  printf("## A random matrix\n");
+  auto x = spar::gen::rand<int, int>(12, .07, 10, 5);
   
   x.info();
   x.print();
   x.print(true);
   
-  printf("## A band-ish matrix\n");
-  auto y = spar::gen::bandish<int, int>(1234, 10, 5);
+  printf("## A banded matrix\n");
+  auto y = spar::gen::banded<int, int>(2, 10, 5);
   
   y.info();
   y.print();
   y.print(true);
+  
+  printf("## A band-ish matrix\n");
+  auto z = spar::gen::bandish<int, int>(1234, 10, 5);
+  
+  z.info();
+  z.print();
+  z.print(true);
   
   return 0;
 }
