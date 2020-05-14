16bb28745c7f12fda5062a630ea3500c08b90c80
wrathematics <wrathematics@gmail.com>
2020-05-13 20:32:09 -0400

    add unstructured random generator

--- src/gen/gen.hpp
+++ src/gen/gen.hpp
@@ -11,6 +11,7 @@
 #include <cstdint>
 #include <random>
 #include <stdexcept>
+#include <vector>
 
 #include "rand.hpp"
 
@@ -20,11 +21,131 @@ class spvec;
 template <typename INDEX, typename SCALAR>
 class spmat;
 
+
 namespace spar
 {
   /// @brief Random generators.
   namespace gen
   {
+    namespace internals
+    {
+      // sample a vector of length `len` with values coming from the interval
+      // [0, n-1]
+      template <typename INDEX>
+      static std::vector<INDEX> res_sampler(const uint32_t seed, const INDEX n,
+        const INDEX len, const bool zero_based_indices=true)
+      {
+        const INDEX offset = (INDEX) 1 - zero_based_indices;
+        
+        std::vector<INDEX> x(len);
+        
+        #pragma omp for simd
+        for (INDEX i=0; i<len; i++)
+          x[i] = i + offset;
+        
+        std::mt19937 mt(seed);
+        
+        #pragma omp for simd
+        for (INDEX i=len; i<n; i++)
+        {
+          std::uniform_int_distribution<INDEX> dist(0, i);
+          INDEX j = dist(mt);
+          
+          if (j < len)
+            x[j] = i + offset;
+        }
+        
+        std::sort(x.begin(), x.end());
+        
+        return x;
+      }
+    }
+    
+    
+    /**
+      @brief Generate a sparse matrix whose entries are randomly
+      TODO
+      
+      @param[in] seed Random seed.
+      @param[in] prop_dense Proportion of density. Should be a number between
+      0 and 1. Other numbers will be truncated to 0 (those less than 0) or 1
+      (those greater than 1).
+      @param[in] nrows,ncols The dimensions of the return.
+      
+      @return A sparse matrix.
+      
+      @allocs The return matrix is allocated/resized as necessary.
+      
+      @except If a memory allocation fails, a `bad_alloc` exception will be
+      thrown.
+     */
+    template <typename INDEX, typename SCALAR>
+    static inline spmat<INDEX, SCALAR> rand(const uint32_t seed,
+      const float prop_dense, const INDEX nrows, const INDEX ncols)
+    {
+      const INDEX nnz = (INDEX) (std::min(std::max(prop_dense, 0.f), 1.f) * nrows*ncols);
+      spmat<INDEX, SCALAR> x(nrows, ncols, nnz);
+      if (nnz == 0)
+        return x;
+      
+      INDEX *I = x.index_ptr();
+      INDEX *P = x.col_ptr();
+      SCALAR *X = x.data_ptr();
+      
+      const INDEX plen = ncols + 1;
+      
+      std::vector<INDEX> indices = internals::res_sampler(seed, nrows*ncols, nnz);
+      INDEX col = 0;
+      for (INDEX ind=0; ind<nnz; ind++)
+      {
+        INDEX i = indices[ind] % nrows;
+        INDEX j = (indices[ind] - i) / nrows;
+        
+        I[ind] = i;
+        X[ind] = 1;
+        
+        if (col == j)
+        {
+          P[col] = ind;
+          col++;
+        }
+        
+        while (col < j)
+        {
+          P[col] = ind;
+          col++;
+        }
+      }
+      
+      for (INDEX ind=col; ind<plen; ind++)
+        P[ind] += nnz;
+      
+      x.update_nnz();
+      
+      return x;
+    }
+    
+    /// \overload
+    template <typename INDEX, typename SCALAR>
+    static inline spmat<INDEX, SCALAR> rand(const float prop_dense,
+      const INDEX nrows, const INDEX ncols)
+    {
+      const uint32_t seed = spar::internals::rand::get_seed();
+      return rand<INDEX, SCALAR>(seed, prop_dense, nrows, ncols);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     namespace internals
     {
       template <typename INDEX>
